d68f6fc1c9e3b772e740fd1fd93965af761bc463
Arthur Moreau <kibtpwd@gmail.com>
2026-04-03 12:10:52 +0200

    Remove SendGrid provider configuration

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,11 @@
 module.exports = () => ({
   email: {
     config: {
-      provider: 'sendgrid', // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
-      providerOptions: {
-        apiKey: env('SENDGRID_API_KEY'),
-      },
       settings: {
         defaultFrom: 'arthur@bwop.io',
         defaultReplyTo: 'arthur@bwop.io',
-        testAddress: 'arthur@bwop.io',
-      },
-    },
-  },
+        testAddress: 'arthur@bwop.io'
+      }
+    }
+  }
 });
